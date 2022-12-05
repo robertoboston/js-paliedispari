c2ef9499de871ce04023105490d67f2682837320
roberto.boston <roberto.bozzo97@tiscali.it>
2022-12-05 16:32:31 +0100

    1

--- pari e dispari/script.js
+++ pari e dispari/script.js
@@ -1,20 +1,14 @@
-let pariDispari = prompt('scegli tra pari e dispari')
-let numeroUtente =parseInt(prompt ('scegli un numero tra 1 e 5')) 
 
 function numberPc(min , max){
+
     let numberRandom = Math.floor(Math.random() * (max - min +1) + min)
     console.log(numberRandom)
 
-    let somma = numeroUtente + numberRandom
-
-    console.log(somma)
-
-    if(somma % 2 == 0){
-        
-    }
-
-
 
 }
+
 numberPc(1,5)
+
+let pariDispari = prompt('scegli tra pari e dispari')
+let numeroUtente =parseInt(prompt ('scegli un numero tra 1 e 5')) 
 
